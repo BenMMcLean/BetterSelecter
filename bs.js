19c75d7b4a66549e46653e39973210f2ccc84fba
Ben McLean <benmclean007@gmail.com>
2016-12-27 21:22:14 +1100

    Cleaned code, changed textbox behavior

--- bs.js
+++ bs.js
@@ -1,11 +1,8 @@
 (function($){
-	var selId;
-	var currentSearch;
-
-	function dsi(){
-		return "[data-selector-id=" + selId + "]";
-	}
-
+	var searchParent;
+	var selectParent;
+	var search;
+	
 	function guid() {
 		function s4() {
 			return Math.floor((1 + Math.random()) * 0x10000)
@@ -14,61 +11,63 @@
 		}
 		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 	}
-
+	
 	function searchFilter(s){
 		return s.name.toLowerCase().indexOf(currentSearch.toLowerCase()) >= 0;
 	}
-
+	
 	function addOps(ops){
-		$(dsi() + " .betterselecter-content").empty();
-
+		$content = searchParent.find(".betterselecter-content");
+		$content.empty();
+		
 		for(var i = 0; i < ops.length; i++){
-			$(dsi() + " .betterselecter-content").append("<div class='betterselecter-op' data-selector-name='"+ ops[i].name +"' data-selector-value='"+ ops[i].value +"'>"+ ops[i].name +"</div>");
+			$content.append("<div class='betterselecter-op' data-selector-name='"+ ops[i].name +"' data-selector-value='"+ ops[i].value +"'>"+ ops[i].name +"</div>");
 		}
-
-		$(dsi() + " .betterselecter-op").click(function(){
-			$(dsi() + " .bs-selected").removeClass("bs-selected");
-			$(this).addClass("bs-selected");
-			$(dsi() + " [data-input]").val($(this).attr("data-selector-name"));
-			$(dsi() + " [data-val]").val($(this).attr("data-selector-value"));
-
-			$(dsi() + " .betterselecter-content").slideUp();
+		
+		searchParent.find(".betterselecter-op").click(function(){
+			searchParent.find(".betterselecter-value").val($(this).attr("data-selector-value"));
+			selectParent.html($(this).attr("data-selector-name"));
+			searchParent.hide();
 		});
 	}
-
+	
 	$.fn.betterSelect = function(){
 		var ops = [];
-
+		
 		this.find("option").each(function(e){
 			ops.push(new op($(this).html(), $(this).val()));
 		});
-
-		selId = guid();
-
-		this.replaceWith("<div class='betterselecter clearfix' data-selector-id='"+ selId +"'><input data-input type='text' value='"+ ops[0].name +"'/><input data-val type='hidden' name='"+ this.attr("name") +"' value='"+ ops[0].value +"'/><div class='betterselecter-content' style='display:none'></div><div class='betterselecter-footer clearfix'><div style='float:right'>Powered by <a href='//arctro.com'>Arctro</a></div></div></div></div>")
-
-		$(dsi() + " [data-input]").click(function(){
+		
+		var selId = guid();
+		
+		this.replaceWith("<div class='betterselecter-sel' data-selid-i='"+ selId +"'>"+ ops[0].name +"</div><div class='betterselecter clearfix' data-selid-s='"+ selId +"' style='display:none'><input data-input type='text'/><input class='betterselect-value' type='hidden' name='"+ this.attr("name") +"' value='"+ ops[0].value +"'/><div class='betterselecter-content'></div><div class='betterselecter-footer clearfix'><div style='float:right'>Powered by <a href='//arctro.com'>Arctro</a></div></div></div></div>")
+		
+		searchParent = $("[data-selid-s="+ selId +"]");
+		selectParent = $("[data-selid-i="+ selId +"]");
+		addOps(ops);
+		
+		selectParent.click(function(){
 			addOps(ops);
-			$(this).select();
-			$(dsi() + " .betterselecter-content").slideDown();
+			searchParent.css("top", selectParent.position().top + "px").show();
+			searchParent.find("[data-input]").focus().val("")
 		});
-
-		$(document).click(function(event) {
-		    if(!$(event.target).closest(dsi() + "").length) {
-		        if($(dsi() + " .betterselecter-content").is(":visible")) {
-		            $(dsi() + " .betterselecter-content").slideUp();
+		
+		$(document).click(function(event) { 
+		    if(!$(event.target).closest(searchParent.selector).length && !$(event.target).closest(selectParent.selector).length) {
+		        if(searchParent.is(":visible")) {
+		        	searchParent.hide();
 		        }
-		    }
+		    }        
 		});
-
-		$(dsi() + " [data-input]").on("change keyup paste", function(){
+		
+		searchParent.find("[data-input]").on("change keyup paste", function(){
 			currentSearch = $(this).val();
 			addOps(ops.filter(searchFilter));
 		})
-
+		
 		return this;
 	};
-
+	
 	function op(name, value){
 		if(typeof value === "undefined"){
 			this.value = name;
